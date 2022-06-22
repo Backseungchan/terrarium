25eb6137445123fcd587f10ce06cac4d3a9e9e1b
Seungchan Back <bsc980504@outlook.com>
2022-06-22 16:21:37 +0900

    add list page

---
+++ terrarium/static/js/list.js
@@ -0,0 +1,7 @@
+// 페이지가 로드되면 현재 페이지(page)에 따라서 카테고리 표시 상태를 바꾼다.
+$(document).ready(function () {});
+
+function updateButton(postnum, category) {
+  location.href =
+    "../updatepage" + "?category=" + category + "&postnum=" + postnum;
+}
